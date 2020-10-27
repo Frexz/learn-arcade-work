9c2bb54cfe6635a884745cfc5b35d7ccb7c48922
Pettersen <Fredrik.Pettersen@mandal.vgs.no>
2020-10-27 09:23:40 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,17 @@
-Hi
+print("""\n\'Twas the night before Christmas, when all through the house
+Not a creature was stirring, not even a mouse;
+The stockings were hung by the chimney with care,
+In hopes that St. Nicholas soon would be there;""")
+print("""\nThe children were nestled all snug in their beds,
+While visions of sugar-plums danced in their heads;
+And mamma in her \'kerchief, and I in my cap,
+Had just settled down for a long winter\'s nap,""")
+print("\n...")
+print("""\nHe spoke not a word, but went straight to his work,
+And filled all the stockings; then turned with a jerk,
+And laying his finger aside of his nose,
+And giving a nod, up the chimney her rose;""")
+print("""\nHe sprang to his sleigh, to his team gave a whistle,
+And away they all flew like the down of a thistle.
+But I heard him exclaim, ere he drove out of sight,
+HAPPY CHRISTMAS TO ALL, AND TO ALL A GOOD-NIGHT!""")
